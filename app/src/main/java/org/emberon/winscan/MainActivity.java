887752c5636d711feebee0aaf4e58126f5034f5c
vaibhavgoel97 <vaibhav16111@iiitd.ac.in>
2020-10-24 14:50:41 +0530

    Updated to master

--- app/src/main/java/org/emberon/winscan/MainActivity.java
+++ app/src/main/java/org/emberon/winscan/MainActivity.java
@@ -1,7 +1,14 @@
 package org.emberon.winscan;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -10,8 +17,13 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.emberon.winscan.home.HomeFragment;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    public final int UPI_PAYMENT = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,4 +39,86 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case UPI_PAYMENT:
+                if ((RESULT_OK == resultCode) || (resultCode == 11)) {
+                    if (data != null) {
+                        String trxt = data.getStringExtra("response");
+                        Log.d("UPI", "onActivityResult: " + trxt);
+                        ArrayList<String> dataList = new ArrayList<>();
+                        dataList.add(trxt);
+                        upiPaymentDataOperation(dataList);
+                    } else {
+                        Log.d("UPI", "onActivityResult: " + "Return data is null");
+                        ArrayList<String> dataList = new ArrayList<>();
+                        dataList.add("nothing");
+                        upiPaymentDataOperation(dataList);
+                    }
+                }
+
+                else {
+                    Log.d("UPI", "onActivityResult: " + "Return data is null"); //when user simply back without payment
+                    ArrayList<String> dataList = new ArrayList<>();
+                    dataList.add("nothing");
+                    upiPaymentDataOperation(dataList);
+                }
+                break;
+        }
+    }
+
+    private void upiPaymentDataOperation(ArrayList<String> data) {
+        if (isConnectionAvailable(this)) {
+            String str = data.get(0);
+            Log.d("UPIPAY", "upiPaymentDataOperation: "+str);
+            String paymentCancel = "";
+            if(str == null) str = "discard";
+            String status = "";
+            String approvalRefNo = "";
+            String response[] = str.split("&");
+            for (int i = 0; i < response.length; i++) {
+                String equalStr[] = response[i].split("=");
+                if(equalStr.length >= 2) {
+                    if (equalStr[0].toLowerCase().equals("Status".toLowerCase())) {
+                        status = equalStr[1].toLowerCase();
+                    }
+                    else if (equalStr[0].toLowerCase().equals("ApprovalRefNo".toLowerCase()) || equalStr[0].toLowerCase().equals("txnRef".toLowerCase())) {
+                        approvalRefNo = equalStr[1];
+                    }
+                }
+                else {
+                    paymentCancel = "Payment cancelled by user.";
+                }
+            }
+
+            if (status.equals("success")) {
+                //Code to handle successful transaction here.
+                Toast.makeText(MainActivity.this, "Transaction successful.", Toast.LENGTH_SHORT).show();
+                Log.d("UPI", "responseStr: "+approvalRefNo);
+            }
+            else if("Payment cancelled by user.".equals(paymentCancel)) {
+                Toast.makeText(MainActivity.this, "Payment cancelled by user.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Transaction failed.Please try again", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.this, "Internet connection is not available. Please check and try again", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static boolean isConnectionAvailable(MainActivity context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.isConnected()
+                    && netInfo.isConnectedOrConnecting()
+                    && netInfo.isAvailable()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- app/src/main/java/org/emberon/winscan/home/HomeFragment.java
+++ app/src/main/java/org/emberon/winscan/home/HomeFragment.java
@@ -1,10 +1,18 @@
 package org.emberon.winscan.home;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +20,87 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.emberon.winscan.MainActivity;
 import org.emberon.winscan.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private Button buttonPay;
+    private TextView textView;
+    private TextInputLayout upiId;
+    private TextInputLayout amount;
+    String upiIdText;
+    String amountText;
+    Activity context;
+    public final int UPI_PAYMENT = 123;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        textView = root.findViewById(R.id.text_home);
+        buttonPay = root.findViewById(R.id.button_pay);
+        upiId = root.findViewById(R.id.upi_id);
+        amount = root.findViewById(R.id.amount);
+        context = getActivity();
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+        buttonPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(upiId.getEditText().getText().toString())) {
+                    Toast.makeText(context, " UPI ID is invalid", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(amount.getEditText().getText().toString())) {
+                    Toast.makeText(context, " Amount is invalid", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    upiIdText = upiId.getEditText().getText().toString();
+                    amountText = amount.getEditText().getText().toString();
+                    textView.setText(upiIdText.concat(amountText));
+                    Log.e("main ", "STEP 1");
+                    payUsingUpi("VAOBHAV", upiIdText,
+                            "TIRAL", amountText);
+                }
+            }
+        });
         return root;
+    }
+
+    void payUsingUpi(  String name,String upiId, String note, String amount) {
+
+        Log.e("main ", "STEP 2");
+        Log.e("main ", "name "+name +"--up--"+upiId+"--"+ note+"--"+amount);
+        Uri uri = Uri.parse("upi://pay").buildUpon()
+                .appendQueryParameter("pa", upiId)
+                .appendQueryParameter("pn", name)
+                .appendQueryParameter("tn", note)
+                .appendQueryParameter("am", amount)
+                .appendQueryParameter("cu", "INR")
+                .build();
+        Intent upiPayIntent = new Intent(Intent.ACTION_VIEW);
+        upiPayIntent.setData(uri);
+        // will always show a dialog to user to choose an app
+        Intent chooser = Intent.createChooser(upiPayIntent, "Pay with");
+
+        // check if intent resolves
+        Log.e("main ", "STEP 3");
+        ComponentName x =  chooser.resolveActivity(context.getPackageManager());
+        System.out.println(x);
+        if(null != x) {
+            Log.e("main ", "STEP INSIDE");
+            context.startActivityForResult(chooser, UPI_PAYMENT);
+        } else {
+            Log.e("main ", "STEP OUTSIDE");
+            Toast.makeText(context,"No UPI app found, please install one to continue", Toast.LENGTH_SHORT).show();
+        }
     }
 }
